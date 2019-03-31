1364c87e8e890dc780020477800d2d6c7e269509
Julian Moorhouse <julesthecoder@gmail.com>
2019-03-31 20:12:50 +0100

    Added files to subtree repo

---
+++ AssemblyInfo.vb
@@ -0,0 +1,34 @@
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("")> 
+<Assembly: AssemblyDescription("Ideaspad Icon Pack")> 
+<Assembly: AssemblyCompany("Mindwarp Consultancy Ltd")> 
+<Assembly: AssemblyProduct("Ideaspad  Icon Pack 2008")> 
+<Assembly: AssemblyCopyright("Copyright� Mindwarp Consultancy Ltd. 2008")>  '2003
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+<Assembly: AssemblyKeyFile("..\..\..\IdeasPad\ideaspad.snk")> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6426FD9C-6104-4B35-90A3-95116F685273")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+'<Assembly: AssemblyVersion("1.0.0")> 
+'<Assembly: AssemblyVersion("2.0.0.0")>  'changed to produce nice error with old icons packs
+<Assembly: AssemblyVersion("2.0.0.1")>  'added move up, down buttons
